5d959e06aadecf6a2b400f535748bf32b98a23fb
piesieomane <piesieyeboah@gmail.com>
2022-10-06 08:56:53 +0000

    Write Test for factorial in solver class

--- spec/solver_helper_spec.rb
+++ spec/solver_helper_spec.rb
@@ -1,1 +1,24 @@
 require_relative '../solver'
+
+describe Solver do
+    describe "#factorial" do
+        it "returns 1 when given 0" do
+            expect(Solver.factorial(0)).to eq(1)
+        end
+        it "returns 1 when given 1" do
+            expect(Solver.factorial(1)).to eq(1)
+        end
+        it "returns 2 when given 2" do
+            expect(Solver.factorial(2)).to eq(2)
+        end
+        it "returns 6 when given 3" do
+            expect(Solver.factorial(3)).to eq(6)
+        end
+        it "returns 24 when given 4" do
+            expect(Solver.factorial(4)).to eq(24)
+        end
+        it "returns 120 when given 5" do
+            expect(Solver.factorial(5)).to eq(120)
+        end
+    end
+end
